ee3dc3bb4d92db6af775dee226b18b11992eef3a
DidaPrasetyo <didaprasetyorahmat@gmail.com>
2023-08-31 01:00:22 +0700

    add int8 yolo model, ssd mobilenetv2 and comment in main.py

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import numpy as np
 import tflite_runtime.interpreter as tflite
 
 # Load the TFLite model and allocate tensors.
-# interpreter = tf.lite.Interpreter(model_path="yolov8n/best_integer_quant.tflite")
+# interpreter = tf.lite.Interpreter(model_path="yolov8n/best_int8.tflite")
 interpreter = tflite.Interpreter("yolov8n/best_integer_quant.tflite",
   experimental_delegates=[tflite.load_delegate('libedgetpu.so.1')])
 interpreter.allocate_tensors()
+
+# best_full use float32 format, not int8
 
 # Get input and output tensors.
 input_details = interpreter.get_input_details()
