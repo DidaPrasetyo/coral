0427bb7f80f46f0a85b956a054049c2b00c3a9fe
DidaPrasetyo <didaprasetyorahmat@gmail.com>
2024-01-05 18:14:03 +0700

    limit the upload image
    because the upload image outside the person checking, it spam the database

--- detect.py
+++ detect.py
@@ -126,11 +126,12 @@ def main():
                     cv2.putText(frame, f"Persons: {detected_persons}", (175, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 1)
                     cv2.putText(frame, f"inference TIme: {(inference_time * 1000):.4f} ms", (350, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 1)
                 
-                dim = (int(args.width), int(args.height))
-                frame = cv2.resize(frame, dim, interpolation=cv2.INTER_AREA)
+                if detected_persons > 0:
+                    dim = (int(args.width), int(args.height))
+                    frame = cv2.resize(frame, dim, interpolation=cv2.INTER_AREA)
 
-                blob_img = convert_image_to_blob(frame)
-                upload_image_to_mysql(args.host, time.strftime('%Y-%m-%d %H:%M:%S'), detected_persons, blob_img)
+                    blob_img = convert_image_to_blob(frame)
+                    upload_image_to_mysql(args.host, time.strftime('%Y-%m-%d %H:%M:%S'), detected_persons, blob_img)
 
                 # count += 1
                 # save_frame_as_image(frame, detected_persons, count)
